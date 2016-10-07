c7b47b51e64d358cbabad9e6c18d7bb24913684a
rojbert <rzablocki@gmail.com>
2016-10-07 09:55:53 +0000

    test

---
+++ views/atrakcje.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.default')
+
+@section('content')
+	<h1>{{ $data->page->title }}</h1>   
+    @include('includes.sections')
+@endsection
---
+++ views/sections/atrakcje.blade.php
@@ -0,0 +1,14 @@
+@extends('sections.layouts.one-column')
+
+@section('section-content-col1') 
+ 
+    @foreach($section->atrakcje as $atrakcja)
+    
+        <div>
+            {{ $atrakcja->name }}<br />
+            {{ $atrakcja->subtitle }}
+        </div>
+        
+    @endforeach
+    
+@overwrite
