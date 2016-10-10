24b256d33c49df32623717c4e2e0e88b5ddca05d
rojbert <rzablocki@gmail.com>
2016-10-10 23:45:59 +0000

    prace nad menu

---
+++ assets/js/scripts.js
@@ -0,0 +1,11 @@
+(function($) {
+    "use strict"; // Start of use strict
+
+    // Offset for Main Navigation
+    $('#main-nav').affix({
+        offset: {
+            top: 100
+        }
+    })
+	
+})(jQuery); // End of use strict
--- views/atrakcje.blade.php
+++ views/atrakcje.blade.php
@@ -1,14 +1,22 @@
 @extends('layouts.default')
 
-<div class="debug-colors">
+@section('content')
+    <div class="full-image">
+
+	<h1><span>{{ $data->page->title }}</span></h1>   
+    
+     </div>
+    
+    @include('includes.sections')
+    
+   
+    <!-- Jeśli w presecie ustawie "show-colors" na true, to powinno to się pojaiwc dopiero -->
+    <div class="debug-colors">
 	<div class="c-1">@dark-color</div>
     <div class="c-2">@light-color</div>
     <div class="c-3">@brand-primary</div>
     <div class="c-4">@brand-success</div>
-</div>
-
-@section('content')
-	<h1>{{ $data->page->title }}</h1>   
-    @include('includes.sections')
+	</div>
+    
 @endsection
 
