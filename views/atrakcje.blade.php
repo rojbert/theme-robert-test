e97eca5b81273fc24875dea99b52ffce70cc4c92
rojbert <rzablocki@gmail.com>
2016-10-10 08:39:58 +0000

    pierwsze testy

--- views/atrakcje.blade.php
+++ views/atrakcje.blade.php
@@ -1,6 +1,14 @@
 @extends('layouts.default')
 
+<div class="debug-colors">
+	<div class="c-1">@dark-color</div>
+    <div class="c-2">@light-color</div>
+    <div class="c-3">@brand-primary</div>
+    <div class="c-4">@brand-success</div>
+</div>
+
 @section('content')
 	<h1>{{ $data->page->title }}</h1>   
     @include('includes.sections')
 @endsection
+
